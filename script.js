fc0656c3a11bc4ec3fa5d71711af0fa057d9f92e
Specialized101 <yassinefares17@gmail.com>
2023-11-08 13:58:42 +0100

    Add basic operations to the calculator

--- script.js
+++ script.js
@@ -11,12 +11,66 @@ const operate = (operator, a, b) => {
         case '*':
             return multiply(a, b);
         case '/':
-            return divide(a ,b);
+            if (b === 0)
+                return 'LOL';
+            return Math.round(divide(a, b) * 100) / 100;
     }
 }
 
+const updateDisplay = () => {
+    const display = document.querySelector('#display');
+    display.textContent = displayValue;
+}
 
-const firstNumber = null;
-const secondeNumber = null;
-const operator = null;
+const buttons = document.querySelectorAll('.btn');
+const numericButtons = Array.from(buttons).filter(button => !isNaN(button.value));
+const operatorButtons = Array.from(buttons).filter(button => isNaN(button.value));
+
+
+let displayValue = '';
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+numericButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayValue = displayValue.concat(button.value);
+        updateDisplay(displayValue);
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.value) {
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                if (operator) {
+                    secondNumber = parseFloat(displayValue);
+                    displayValue = operate(operator, firstNumber, secondNumber);
+                }
+                operator = button.value;
+                firstNumber = parseFloat(displayValue);
+                updateDisplay();
+                displayValue = '';
+                break;
+            case 'clear':
+                displayValue = '';
+                firstNumber = null;
+                secondNumber = null;
+                operator = null;
+                updateDisplay();
+                break;
+            case '=':
+                secondNumber = parseFloat(displayValue);
+                displayValue = operate(operator, firstNumber, secondNumber);
+                operator = null;
+                updateDisplay();
+                break;
+        }
+    });
+});
+
 
