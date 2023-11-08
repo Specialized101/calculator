4907a761c7c6d903a865fcbad962486a78b18078
Specialized101 <yassinefares17@gmail.com>
2023-11-08 11:29:22 +0100

    Add all the buttons to the page

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return substract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a ,b);
+    }
+}
+
+
+const firstNumber = null;
+const secondeNumber = null;
+const operator = null;
+
